4ee6a51e44ae14b04d969d0a534abeec7c115276
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-05 20:13:27 -0500

    updated ajax file to include both post and get

--- express/ajax.js
+++ express/ajax.js
@@ -1,25 +1,28 @@
+var createCard = data => {
+    return `<div class="card">
+           <div class="card-body">
+            data
+           </div>
+        </div>`
+
+}
 $(document).ready(function () {
     $("#current").submit(function (event) {
-        // Stop form from submitting normally
         event.preventDefault();
 
-        /* Serialize the submitted form control values to be sent to the web server with the request */
         var formValues = $(this).serialize();
-
-        var createCard = data => {
-            return `<div class="card">
-                   <div class="card-body">
-                    data
-                   </div>
-                </div>`
-
-        }
 
         // Send the form data using post
         $.post("reservation.html", formValues, function (data) {
-            // Display the returned data in browser
+            console.log(data)
             var render = createCard(data);
             $("#current").html(render);
         });
     });
 });
+
+$.get("/api/reservations", res => {
+    console.log(res);
+    // $("#current").html(JSON.parse(res));
+
+})
